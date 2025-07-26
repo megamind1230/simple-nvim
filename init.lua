c202ae3bbb17ed81731b750196b33a097d112bdc
megamind1230 <hassan24641@gmail.com>
2025-07-27 01:20:45 +0300

    27-7-2025

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
-require("config.lazy")
-require("keymaps")
-require("options")
+-- sourcing {grap and connect} the config separate files to the starting point {init.lua}
+require("config.lazy") --lazy FIRST
+--require("folder.file")
+--or: require("folder/file")
+require("keymaps") --files inside lua/ folder .. can be sourced directly like this
+require("options") -- same as: require("lua.options")
 require("colorscheme")
--- lua/options.lua
+++ lua/options.lua
@@ -2,32 +2,33 @@
 local op= vim.o --faster
 
 	--linenumber
-vim.wo.relativenumber = false
 op.number = true
-op.ruler = true
+vim.wo.relativenumber = false --better be off if u do this too much => :65 {go to line 65}
+op.ruler = false -- true to show the cursor position on the file
 
 	--indent, tab, space
-op.tabstop = 2
+op.expandtab = true --convert \t into spaces --btw bad for a MakeFile
+op.tabstop = 2 -- \t would be 2 spaces wide
 op.softtabstop = 2
-op.shiftwidth = 2
-op.expandtab = true
+op.shiftwidth = 2 -- using >> or << would be 2 spaces wide
 op.smartindent = true
 
 	--search
-op.ignorecase = true
-op.smartcase = true
-op.hlsearch = true
+op.ignorecase = true -- /hello matches Hello hello heLLoW
+op.smartcase = true -- but /Hello would only match Hello
+op.hlsearch = true 
 
   --good visuals
-op.cursorline = true
-op.signcolumn = "yes"
-op.wrap = true
+op.cursorline = true -- highlight the line the curson on
+op.signcolumn = "yes" --left sidebar space for lsp, breakepoints, git changes ..etc
+op.wrap = true --break the line if too long
+op.linebreak = true -- properly
 op.splitbelow = true
 op.splitright = true
-op.timeoutlen = 300
+op.timeoutlen = 300 -- not to leave you hanging too much
 
   --clipboard
-op.clipboard = "unnamedplus"
+op.clipboard = "unnamedplus" -- use system clipboard instead
 
   --undofile
 -- op.undofile = true
